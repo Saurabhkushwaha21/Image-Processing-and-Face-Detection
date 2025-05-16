4e3cb6c08b04d5261bc1bd80481aa25b71f70272
saurabh kushwaha <kushwahasaurabh639@gmail.com>
2025-05-16 22:56:04 +0530

    Update second.py

--- second.py
+++ second.py
@@ -1,14 +1,14 @@
-#import math
-# print(math.sqrt(9))
-# print(math.ceil(9.4))
-# print(math.floor(9.4))
-#print(math.sin(45))
+import math
+print(math.sqrt(9))
+print(math.ceil(9.4))
+print(math.floor(9.4))
+print(math.sin(45))
 
-# single function find in math library
-# from math import sqrt,ceil
-# print(sqrt(144))
-# print(ceil(16.6))
+single function find in math library
+from math import sqrt,ceil
+print(sqrt(144))
+print(ceil(16.6))
 
-#if we use function name create
-# from math import ceil as ce
-# print(ce(16.6))
+if we use function name create
+from math import ceil as ce
+print(ce(16.6))
