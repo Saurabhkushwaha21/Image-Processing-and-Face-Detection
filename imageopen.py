3dfac6be2c8061c597ab954a999fad774c316f74
saurabh kushwaha <kushwahasaurabh639@gmail.com>
2025-05-16 22:55:13 +0530

    Update imageopen.py

--- imageopen.py
+++ imageopen.py
@@ -10,18 +10,17 @@ import cv2
 import cv2.data
 import os
 print(os.listdir(cv2.data.haarcascades))
-# image=cv2.imread("group.jpg")
-# filename="haarcascade_frontalface_default.xml"
-# path=cv2.data.haarcascades+filename
-# model=cv2.CascadeClassifier(path)
-# faces=model.detectMultiScale(image,1.3,5)
-# for face in faces:
-    # x1=face[0]
-    # y1=face[1]
-    # x2=x1+face[2]
-    # y2=y1+face[3]
-# 
-    # cv2.rectangle(image,(x1,y1),(x2,y2),[255,255,255],1)
-# cv2.imshow("person",image)
-# cv2.waitKey(0)
-#cv2.destroyAllWindow()
+ image=cv2.imread("group.jpg")
+ filename="haarcascade_frontalface_default.xml"
+ path=cv2.data.haarcascades+filename
+ model=cv2.CascadeClassifier(path)
+ faces=model.detectMultiScale(image,1.3,5)
+ for face in faces:
+     x1=face[0]
+     y1=face[1]
+     x2=x1+face[2]
+     y2=y1+face[3]
+     cv2.rectangle(image,(x1,y1),(x2,y2),[255,255,255],1)
+ cv2.imshow("person",image)
+ cv2.waitKey(0)
+cv2.destroyAllWindow()
